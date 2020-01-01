89c492e573810e3415400bb2cd75c557f7bbcf6b
Arpit Bajaj <arpitbajaj98@gmail.com>
2020-01-01 23:59:53 +1100

    Only clickable when youtube video is active in background.js file

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([youtubeRule])
+  })
+})
+
+let youtubeRegex = "^(https?://)?(www.youtube.com|youtu.?be)/.+$"
+
+let youtubeRule = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { urlMatches: youtubeRegex }
+    })
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()]
+}
