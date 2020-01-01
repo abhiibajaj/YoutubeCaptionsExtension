d404d08a3470b091cfaf76cd844d8810d611fcbb
Arpit Bajaj <arpitbajaj98@gmail.com>
2020-01-02 04:24:30 +1100

    Event listener to grab things from the textarea

--- popup.js
+++ popup.js
@@ -1,9 +1,18 @@
-window.onload = function() {
-  document.querySelector("button").addEventListener("click", function() {
-    chrome.identity.getAuthToken({ interactive: true }, function(token) {})
-  })
-}
-
 //Need to get the inner text of the textarea
+let searchContent = null
+document.querySelector("button").addEventListener("click", function() {
+  searchContent = document.getElementById("searchContent").value
+})
+
+document.querySelector("button").addEventListener("click", function() {
+  chrome.identity.getAuthToken({ interactive: true }, function(token) {
+    if (searchContent) {
+      console.log("THE TEXT AREA SAYS " + searchContent)
+    } else {
+      //THROW SOME EXCEPTION
+    }
+  })
+})
+
 //Need to get the youtube id from the current tab
 //Use API
