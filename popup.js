afe6dda51d0f91956855760ba158d2229929bd4c
Arpit Bajaj <arpitbajaj98@gmail.com>
2020-01-06 06:40:55 +1100

    Redirecting to the url when clicked and refreshing the list when new keyword is entered

--- popup.js
+++ popup.js
@@ -12,8 +12,14 @@ document.querySelector("button").addEventListener("click", async function() {
       tab = tabs[0].url
       let youtubeVideoId = tab.split("v=")[1]
       let ampersandPosition = youtubeVideoId.indexOf("&")
+      let youtubeVideo = ""
       if (ampersandPosition != -1) {
         youtubeVideoId = youtubeVideoId.substring(0, ampersandPosition)
+      }
+
+      ampersandPosition = tab.indexOf("&")
+      if (ampersandPosition != -1) {
+        youtubeVideo = tab.substring(0, ampersandPosition)
       }
 
       //Need to get the youtube id from the current tab
@@ -28,21 +34,21 @@ document.querySelector("button").addEventListener("click", async function() {
 
       let corsProxy = "https://cors-anywhere.herokuapp.com/"
       let youtubePlaceholder = "https://video.google.com/timedtext?lang=en&v="
-      // TODO get the ID from the current tab
       let youtubeUrl = youtubePlaceholder + youtubeVideoId
       fetch(corsProxy + youtubeUrl, initial)
         .then(response => response.text())
         .then(transcript => {
           let timedTranscript = new Transcript(transcript)
           let searchResults = timedTranscript.FindQuery(searchContent)
-          addSearchItemsToPopup(tab, searchResults)
+          addSearchItemsToPopup(youtubeVideo, searchResults)
         })
     })
   } else {
-    //THROW SOME EXCEPTION
+    //THROW SOME EXCEPTION OR DISPLAY TO USER
   }
 })
 
+// Not sure how to make this work
 function convertResponseToText(response) {
   return new Promise(resolve => {
     response.text()
@@ -52,6 +58,7 @@ function convertResponseToText(response) {
 
 let addSearchItemsToPopup = (youtubeUrl, searchResults) => {
   let matcheshtml = document.getElementById("listmatches")
+  refreshList(matcheshtml)
   searchResults.forEach(result => {
     let timedUrl = youtubeUrl + "&t=" + result + "s"
     let listElement = document.createElement("li")
@@ -59,8 +66,17 @@ let addSearchItemsToPopup = (youtubeUrl, searchResults) => {
     let linkElement = document.createElement("a")
     linkElement.setAttribute("href", timedUrl)
     linkElement.innerText = timedUrl
+    linkElement.onclick = redirectToNewUrl
     listElement.appendChild(linkElement)
 
     matcheshtml.appendChild(listElement)
   })
+}
+
+let refreshList = matcheshtml => {
+  matcheshtml.innerHtml = ""
+}
+
+let redirectToNewUrl = event => {
+  chrome.tabs.update({ url: event.srcElement.text })
 }
